7b3a555504929739ba7378861c501b49a342daea
Kunal Sambyal <kunalsambyal9628@gmail.com>
2026-06-06 09:52:22 +0530

    Add javascript functinality

---
+++ script.js
@@ -0,0 +1,46 @@
+const inputField = document.getElementById("input-field");
+const addButton = document.getElementById("add-button");
+const list = document.getElementById("list");
+const errorMsg = document.getElementById("error-msg");
+
+function showError(message) {
+    errorMsg.textContent = message;
+    errorMsg.style.visibility = "visible";
+}
+
+function addTask() {
+    const newLi = document.createElement("li");
+    newLi.textContent = inputField.value.trim();
+
+    const newBtn = document.createElement("button");
+    newBtn.classList.add("delete-task");
+    newBtn.textContent = "✖";
+    newLi.appendChild(newBtn);
+
+    list.appendChild(newLi);
+
+    inputField.value = "";
+}
+
+if (addButton) {
+    addButton.addEventListener("click", (event) => {
+        try {
+            if (inputField.value.trim() === "") {
+                throw new Error("Please enter task!");
+            }
+            errorMsg.style.visibility = "hidden";
+            addTask();
+        } catch (error) {
+            console.error(error);
+            showError(error.message);
+        }
+    });
+}
+
+list.addEventListener("click", (event) => {
+    if (event.target.tagName === "LI") {
+        event.target.classList.toggle("checked");
+    } else if (event.target.tagName === "BUTTON") {
+        event.target.parentElement.remove();
+    }
+});
